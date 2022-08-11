f6628d68bed2ee637ad27364836a011f6925b6b9
Timaio <timeiqu@gmail.com>
2022-08-11 18:20:51 +0300

    Initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+  
+  before_action :set_question, only: %i[update show destroy edit]
+
+  def create
+    question = Question.create(question_params)
+    
+    redirect_to question_path(question)
+  end
+
+  def update
+    @question.update(question_params)
+
+    redirect_to question_path(@question)
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  def hide
+    @question = Question.find(params[:id])
+    @question.update(hidden: true)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id, :hidden)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+end
