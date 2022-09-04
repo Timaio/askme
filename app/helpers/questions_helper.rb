7ca5d1ca471e4030c916b7df5d862b40eff839ab
Timaio <timeiqu@gmail.com>
2022-09-04 08:15:38 +0300

    Hashtags display fixed

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def render_with_hashtags(text)
+    text.gsub(/(?:#([\wа-яё]+))/i) { hashtag_link($1) }.html_safe
+  end  
+
+  def hashtag_link(hash)
+    link_to "##{hash}", search_path(query: hash)
+  end
 end
